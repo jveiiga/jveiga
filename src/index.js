381e4fb8f857a86a01cf46528b9c9e5a64561f71
jveiiga <jeferson.veiiga@icloud.com>
2025-03-05 13:20:21 -0300

    Descomentando

--- src/index.js
+++ src/index.js
@@ -3,39 +3,39 @@ import ReactDOM from 'react-dom/client'; // Alteração para importar de 'react-
 import App from './App';
 import LoadingScreen from './components/LoadingScreen/LoadingScreen';
 
-// const Root = () => {
-//   const [loading, setLoading] = useState(true);
+const Root = () => {
+  const [loading, setLoading] = useState(true);
 
-//   useEffect(() => {
-//     const timer = setTimeout(() => {
-//       setLoading(false);
-//     }, 5000);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 5000);
 
-//     return () => clearTimeout(timer);
-//   }, []);
+    return () => clearTimeout(timer);
+  }, []);
 
-//   return (
-//     <React.StrictMode>
-//       {loading ? (
-//         <LoadingScreen />
-//       ) : (
-//         <App />
-//       )}
-//     </React.StrictMode>
-//   );
-// };
+  return (
+    <React.StrictMode>
+      {loading ? (
+        <LoadingScreen />
+      ) : (
+        <App />
+      )}
+    </React.StrictMode>
+  );
+};
 
 // // Alteração aqui para usar 'createRoot' em vez de 'render'
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(<Root />);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Root />);
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 
 
