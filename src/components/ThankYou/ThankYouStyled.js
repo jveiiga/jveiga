2494ce1b19eee7bc139b171695597b2b9816521b
jveiiga <jeferson.veiiga@icloud.com>
2025-03-24 10:27:26 -0300

    Refatorando componente

--- src/components/ThankYou/ThankYouStyled.js
+++ src/components/ThankYou/ThankYouStyled.js
@@ -23,17 +23,6 @@ const slideInFromLeft = keyframes`
   }
 `;
 
-const slideInFromTop = keyframes`
-  from {
-    opacity: 0;
-    transform: translateY(-100%);
-  }
-  to {
-    opacity: 1;
-    transform: translateY(0);
-  }
-`;
-
 const fadeIn = keyframes`
   from {
     opacity: 0;
