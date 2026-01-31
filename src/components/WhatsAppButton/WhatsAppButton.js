beed15cca738c5f4ed4580f3011ca55f2fba264e
jveiiga <jeferson.veiiga@icloud.com>
2026-01-31 19:06:58 -0300

    Enviando Modificações

--- src/components/WhatsAppButton/WhatsAppButton.js
+++ src/components/WhatsAppButton/WhatsAppButton.js
@@ -2,18 +2,18 @@ import React from "react";
 import { FaWhatsapp } from "react-icons/fa";
 import { FloatingButton } from "./WhatsAppButton.styled";
 
-
 const WhatsAppButton = () => {
   return (
     <FloatingButton
-       id="btn_whatsapp"
+      id="btn_whatsapp"
       href="https://wa.me/5511915181487"
       target="_blank"
       rel="noreferrer"
+      onclick="dataLayer.push({event:'click_link', button_id:'btn_whatsapp'})"
       variant="whatsapp"
       aria-label="Fale conosco no WhatsApp"
     >
-     <FaWhatsapp size={26} />
+      <FaWhatsapp size={26} />
     </FloatingButton>
   );
 };
